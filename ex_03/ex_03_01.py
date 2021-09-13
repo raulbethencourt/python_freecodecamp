4386c1865504ad80f4fca0d3cecb7c3f145fe127
Raul Bethencourt <raul.bethencourt.pro@gmail.com>
2021-09-13 09:50:51 +0200

    🚧 finish exo 03_01

--- ex_03/ex_03_01.py
+++ ex_03/ex_03_01.py
@@ -1,4 +1,18 @@
-xh = input("Enter Hours: ")
-xr = input("Enter Rate: ")
-xp = float(xh) * float(xr)
-print(xp)
+sh = input("Enter Hours: ")
+sr = input("Enter Rate: ")
+fh = float(sh)
+fr = float(sr)
+# print(fh, fr)
+
+if fh > 40:
+    # print("Overtime")
+    reg = fh * fr
+    otp = (fh - 40.0) * (fr * 0.5)
+    # print(reg, otp)
+    xp = reg + otp
+else:
+    # print("Regular")
+    xp = fh * fr
+
+print("Pay:", xp)
+
