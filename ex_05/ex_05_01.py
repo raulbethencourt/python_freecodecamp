34e9f187b950700b8e53ec9b4cd30efe2be1f7c7
Raul Bethencourt <raul.bethencourt.pro@gmail.com>
2021-09-24 22:10:59 +0200

    🚧 finish 05 exos

---
+++ ex_05/ex_05_01.py
@@ -0,0 +1,15 @@
+sum = 0
+count = 0
+
+while True:
+    number = input("Enter a number: ")
+    if number == "done":
+        break
+    try:
+        sum += float(number)
+        count += 1
+    except:
+        print("Invalid input")
+        continue
+
+print(sum, count, sum / count)
