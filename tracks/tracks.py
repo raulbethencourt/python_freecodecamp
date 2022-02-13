5a181100afaadc2fd152df954ed55bdbcfc34ab5
raBeta <raul.bethencourt.pro@gmail.com>
2022-02-13 22:22:44 +0100

    :construction: add tracks to sqlite3

--- tracks/tracks.py
+++ tracks/tracks.py
@@ -68,3 +68,17 @@ for entry in all:
     cur.execute("INSERT OR IGNORE INTO Artist (name) VALUES (?)", (artist,))
     cur.execute("SELECT id FROM Artist WHERE name = ?", (artist,))
     artist_id = cur.fetchone()[0]
+
+    cur.execute(
+        "INSERT OR IGNORE INTO Album (title, artist_id) VALUES (?, ?)",
+        (album, artist_id),
+    )
+    cur.execute("SELECT id FROM Album WHERE title = ?", (album,))
+    album_id = cur.fetchone()[0]
+
+    cur.execute(
+        "INSERT OR REPLACE INTO Track (title, album_id, len, rating,count) VALUES (?, ?, ?, ?, ?)",
+        (name, album_id, length, rating, count),
+    )
+
+    conn.commit()
